7e65c0f3b0250c02106adacde9356d9df3fe5c74
floatsd <float.hellowmr@gmail.com>
2017-05-02 22:47:18 +0800

    adjust heatmap

--- 4 heatmap.R
+++ 4 heatmap.R
@@ -5,17 +5,22 @@ library(grid)
 library(RColorBrewer)
 library(Cairo)
 
+f_linearnorm2<-function(feature){
+  delta=max(feature)-min(feature)
+  temp<-(feature-min(feature))/delta
+  return(temp)
+}
 
 
 # set path and output file names
 path="E:/git/plot-yelp"
-output.name<-"heatmap"
+output.name<-"heatmap_reverse_sharedY"
 
 
 # set output file size and font zooming level
 plot.title=""
 g.width=16
-g.height=9
+g.height=6
 k=2
 
 setwd(path)
@@ -34,53 +39,64 @@ rownames(Value2)<-c('CN','Salton','Jaccard','HPI','HDI','AA','RA','Sorensen','LH
 
 
 for (i in 1:6){
-  Value1[,i]<-rank(Value1[,i])
-  Value2[,i]<-rank(Value2[,i])
+  Value1[,i]<-f_linearnorm2(Value1[,i])
+  Value2[,i]<-f_linearnorm2(Value2[,i])
 }
 
 
 melted_Value1<- melt(Value1)
 g1<-ggplot(data=melted_Value1, aes(x=Var2, y=Var1,fill=value))+
   geom_tile()+
-  labs(title="", x = "(a)",y = "")+
+  labs(title="", x = "",y = "(a)")+
   scale_x_discrete(expand = c(0, 0))+
   theme()+
-  theme(
+  theme(      
+    legend.title=element_blank(),
     legend.text = element_text(size=k*10),
-    legend.position='none',
+    # legend.position="top",
+    legend.position = "none",
     plot.title = element_text(size=k*15,hjust = 0.5),
-    axis.text.x=element_text(angle=15,hjust=0.55,vjust=0.8,size=k*10),
+    axis.text.x=element_text(angle=45,hjust=0.55,vjust=0.8,size=k*10),
     axis.text.y = element_text(angle=45,hjust=0.9,size = k*10),
     axis.title.x = element_text(size = k*15),
     axis.title.y = element_text(size = k*15),
     panel.border = element_rect(size = 2,fill=NA)
-    )+
+    )+  
   scale_fill_gradientn(
-    #    low="#ccffcc", high='#009900',
-    colours=colorRampPalette(brewer.pal(9,"RdYlGn"))(20),
-    limits = c(1,19), breaks = c(1, 5, 10, 15, 19))
+      #    low="#ccffcc", high='#009900',
+      colours=colorRampPalette(brewer.pal(4,"Greens"))(20),
+      limits =c(-0.1,1.1), breaks = seq(0,1,length.out = 6))+
+  guides(fill = guide_colorbar(barwidth = 32))+
+  coord_flip()
+
 
 ###############################################################
 melted_Value2<- melt(Value2)
 g2<-ggplot(data=melted_Value2, aes(x=Var2, y=Var1,fill=value))+
   geom_tile()+
-  labs(title="",x = "(b)",y = "")+
+  labs(title="",x = "",y = "(b)")+
   scale_x_discrete(expand = c(0, 0))+
   theme(      
     legend.title=element_blank(),
+    legend.key = element_rect(colour = "black"),
+    legend.position=c(.6,.65),
+    # legend.position='top',
     legend.text = element_text(size=k*10),
     plot.title = element_text(size=k*15,hjust = 0.5),    
-    axis.text.x=element_text(angle=15,hjust=0.55,vjust=0.8,size=k*10),
+    axis.text.x=element_text(angle=45,hjust=0.55,vjust=0.8,size=k*10),
+    axis.text.y = element_blank(),  
+    # axis.text.y = element_text(angle=45,hjust=0.9,size = k*10),
+    
     panel.border = element_rect(size = 2,fill=NA),
-    axis.text.y = element_blank(),
     axis.title.x = element_text(size = k*15),
     axis.title.y = element_text(size = k*15)
     )+
-  guides(fill = guide_colorbar(barwidth = 1,barheight = 20))+
+  guides(fill = guide_colorbar(barwidth = 2,barheight = 17))+
   scale_fill_gradientn(
-#    low="#ccffcc", high='#009900',
-    colours=rev(colorRampPalette(brewer.pal(9,"RdYlGn"))(20)),
-    limits = c(1,19), breaks = c(1, 5, 10, 15, 19))
+    colours=colorRampPalette(brewer.pal(4,"Greens"))(20),
+    limits = c(-0.1,1.1), breaks = seq(0,1,length.out = 6))+
+  coord_flip()
+
 
 legend <- get_legend(g2)
 g2<-g2+theme(legend.position = "none")
@@ -89,5 +105,6 @@ g2<-g2+theme(legend.position = "none")
 cairo_ps(paste("plot/heatmap/",paste(output.name,Sys.Date(),sep="-"),".eps",sep=""),
          family = "Times",width = g.width, height = g.height)
 grid.result<-grid.arrange(g1, g2, legend, ncol=3,
-                          widths = c(2.5, 2,0.4))
+                          widths = c(2.5, 2, 0.4))
+# grid.result<-grid.arrange(g1, g2,  ncol=2,widths = c(2.4, 2))
 dev.off()
