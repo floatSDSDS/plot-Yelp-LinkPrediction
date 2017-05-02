1ff129ed57b378993652b1b1c70907018e02b024
floatsd <float.hellowmr@gmail.com>
2017-05-02 22:57:31 +0800

    add new of heatmaps

--- 4 heatmap.R
+++ 4 heatmap.R
@@ -14,13 +14,13 @@ f_linearnorm2<-function(feature){
 
 # set path and output file names
 path="E:/git/plot-yelp"
-output.name<-"heatmap_reverse_sharedY"
+output.name<-"heatmap_reverse"
 
 
 # set output file size and font zooming level
 plot.title=""
 g.width=16
-g.height=6
+g.height=9
 k=2
 
 setwd(path)
@@ -47,16 +47,16 @@ for (i in 1:6){
 melted_Value1<- melt(Value1)
 g1<-ggplot(data=melted_Value1, aes(x=Var2, y=Var1,fill=value))+
   geom_tile()+
-  labs(title="", x = "",y = "(a)")+
+  labs(title="", x = "(a)",y = "")+
   scale_x_discrete(expand = c(0, 0))+
   theme()+
   theme(      
     legend.title=element_blank(),
     legend.text = element_text(size=k*10),
     # legend.position="top",
-    legend.position = "none",
+    # legend.position = "none",
     plot.title = element_text(size=k*15,hjust = 0.5),
-    axis.text.x=element_text(angle=45,hjust=0.55,vjust=0.8,size=k*10),
+    axis.text.x=element_text(angle=45,hjust=0.55,vjust=0.6,size=k*10),
     axis.text.y = element_text(angle=45,hjust=0.9,size = k*10),
     axis.title.x = element_text(size = k*15),
     axis.title.y = element_text(size = k*15),
@@ -66,7 +66,7 @@ g1<-ggplot(data=melted_Value1, aes(x=Var2, y=Var1,fill=value))+
       #    low="#ccffcc", high='#009900',
       colours=colorRampPalette(brewer.pal(4,"Greens"))(20),
       limits =c(-0.1,1.1), breaks = seq(0,1,length.out = 6))+
-  guides(fill = guide_colorbar(barwidth = 32))+
+  guides(fill = guide_colorbar(barwidth=2,barheight  = 34))#+
   coord_flip()
 
 
@@ -74,37 +74,37 @@ g1<-ggplot(data=melted_Value1, aes(x=Var2, y=Var1,fill=value))+
 melted_Value2<- melt(Value2)
 g2<-ggplot(data=melted_Value2, aes(x=Var2, y=Var1,fill=value))+
   geom_tile()+
-  labs(title="",x = "",y = "(b)")+
+  labs(title="",x = "(b)",y = "")+
   scale_x_discrete(expand = c(0, 0))+
   theme(      
     legend.title=element_blank(),
     legend.key = element_rect(colour = "black"),
-    legend.position=c(.6,.65),
+    # legend.position=c(.6,.6),
     # legend.position='top',
     legend.text = element_text(size=k*10),
     plot.title = element_text(size=k*15,hjust = 0.5),    
-    axis.text.x=element_text(angle=45,hjust=0.55,vjust=0.8,size=k*10),
-    axis.text.y = element_blank(),  
-    # axis.text.y = element_text(angle=45,hjust=0.9,size = k*10),
+    axis.text.x=element_text(angle=45,hjust=0.55,vjust=0.6,size=k*10),
+    # axis.text.y = element_blank(),  
+    axis.text.y = element_text(angle=45,hjust=0.9,size = k*10),
     
     panel.border = element_rect(size = 2,fill=NA),
     axis.title.x = element_text(size = k*15),
     axis.title.y = element_text(size = k*15)
     )+
-  guides(fill = guide_colorbar(barwidth = 2,barheight = 17))+
+  guides(fill = guide_colorbar(barwidth = 2,barheight = 34))+
   scale_fill_gradientn(
     colours=colorRampPalette(brewer.pal(4,"Greens"))(20),
-    limits = c(-0.1,1.1), breaks = seq(0,1,length.out = 6))+
+    limits = c(-0.1,1.1), breaks = seq(0,1,length.out = 6))#+
   coord_flip()
 
 
 legend <- get_legend(g2)
-g2<-g2+theme(legend.position = "none")
+# g2<-g2+theme(legend.position = "none")
 
 # export as eps
 cairo_ps(paste("plot/heatmap/",paste(output.name,Sys.Date(),sep="-"),".eps",sep=""),
          family = "Times",width = g.width, height = g.height)
-grid.result<-grid.arrange(g1, g2, legend, ncol=3,
-                          widths = c(2.5, 2, 0.4))
-# grid.result<-grid.arrange(g1, g2,  ncol=2,widths = c(2.4, 2))
+# grid.result<-grid.arrange(g1, g2, legend, ncol=3,
+                          # widths = c(2.5, 2.5, 0.4))
+grid.result<-grid.arrange(g1, g2,  ncol=2,widths = c(2, 2))
 dev.off()
